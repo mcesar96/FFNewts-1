8746482d39c383000689d2fd833974b91107576b
mary <triny1224@gmail.com>
2019-04-05 19:12:28 -0400

    add weapon
    added water blast (connected to backpack)
    created backpack
    created hose
    created bullet
    created barrel
    created destroyintime

---
+++ Assets/Scripts/BackpackControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackpackControl : MonoBehaviour
+{
+    float dirX, getHoseUp;
+    public float moveSpeed = 5f;
+    Rigidbody2D rb;
+    bool facingRight = true;
+    public Transform hose;
+    Vector3 localScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localScale = transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal");
+        CheckIfHoseUp();
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -8, 8), transform.position.y);
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX * moveSpeed, 0);
+    }
+    void LateUpdate()
+    {
+        CheckWhereToFace();
+    }
+    void CheckWhereToFace()
+    {
+        if (dirX > 0)
+            facingRight = true;
+        else
+            if (dirX < 0)
+            facingRight = false;
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+            localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+    void CheckIfHoseUp()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            hose.rotation = Quaternion.Euler(0, 0, 45 * localScale.x);
+        else
+            hose.rotation = Quaternion.Euler(0, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+
+    float fireRate;
+    float nextFire;
+
+    // using this for initialization
+    void Start()
+    {
+        fireRate = 1f;
+        nextFire = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfToFire();
+    }
+    void CheckIfToFire()
+    {
+        if(Time.time > nextFire)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
