4b3207deea63a9f1b70b90adc5055ef80c8ffd31
Marcus <marcuscesar1996@gmail.com>
2019-04-06 12:26:30 -0400

    Parallax Update
    The parallax didn't look realisitc. Made updates to keep the background more stationary whilst the foreground moves

--- Assets/Scripts/Parallaxing.cs
+++ Assets/Scripts/Parallaxing.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Parallaxing : MonoBehaviour
 {
     public Transform[] backgrounds; // Array (list) of all the back and foregrounds to be parallaxed
-    private float[] parallaxScales; // The proportion of the camera's movement to mvoe the backgrounds by.
+    private float[] parallaxScales; // The proportion of the camera's movement to move the backgrounds by.
     public float smoothing = 1f;    // How smooth the parallax is going to be. Make sure to set this above 0.
 
     private Transform cam;          // reference to the main cameras transform
