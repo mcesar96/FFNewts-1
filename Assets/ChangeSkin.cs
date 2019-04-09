365f2cfff1dc0f1308537e65868749b18f1de7dc
Emilio Sillart <esillart314@gmail.com>
2019-04-09 01:34:33 -0400

    added skin change feature when pressing newt in menu
    cycles between 4 skins, slight error where it'll cycle to the red skin twice but only in the first cycle

---
+++ Assets/ChangeSkin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSkin : MonoBehaviour{
+
+    public Sprite[] sprite;
+    public Button button;
+
+    int count = 0;
+
+    void Awake() {
+        //this code allows component to recognize the sprites
+        sprite = Resources.LoadAll<Sprite>("newt_skins");
+    }
+
+    public void On_Click_Button() {
+        count++;
+
+        if (count == sprite.Length)
+            count = 0;
+
+        button.image.sprite = sprite[count];
+    }
+    
+}
