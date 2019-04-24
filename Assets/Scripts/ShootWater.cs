771ae9e41acece3d7e46b02bde07af49c9d11721
Tan arin <tanarin12@gmail.com>
2019-04-24 15:47:14 -0400

    Finish adding water stream
    The water stream is now added. It shoots in the direction of the
    pointer, as well as has different pressure for how far the pointer is.
    To makes the water stream I used a particle system.

--- Assets/Scripts/ShootWater.cs
+++ Assets/Scripts/ShootWater.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ShootWater : MonoBehaviour
 {
     public GameObject player;
+
+
+    Vector3 direction; //for location of mouse
+
     ParticleSystem waterParticles;
+    Vector2 waterPosition;
+    private float maxWaterSpeed = 10.0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +25,23 @@ public class ShootWater : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y));
-        Vector2 myPos = new Vector2(transform.position.x, transform.position.y + 1);
-        Vector2 direction = target - myPos;
-        direction.Normalize();
+        {
+            //Makes water Stream point towards mouse
+            var pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));    //gets position of mouse in World
 
+            waterParticles.transform.LookAt(pos);   //makes particles rotation aim towards the mouse (which is pos)
+
+            //Makes water stream faster/slower based on cursor distance
+            var waterStrength = waterParticles.main;    //grabs the particle system main component
+            
+                //Makes sure there theres a minimum water pressure and maximum
+            //if(waterStrength.startSpeedMultiplier < 10 && waterStrength.startSpeedMultiplier > 5)
+            waterStrength.startSpeedMultiplier = Vector3.Distance(player.transform.position, pos) * 3;  //multiplies the start speed by distance of mouse * 4 (the 4 is to normalize)
+        }
+
+
+        //Next line makes water stream same location as player
         transform.position = new Vector2(player.transform.position.x, player.transform.position.y);
 
-        waterParticles.transform.rotation.x = 
     }
 }
---
+++ Assets/Scripts/WaterInstantiate.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterInstantiate : MonoBehaviour
+{
+    public Transform barrel;
+    public ParticleSystem waterStream;
+
+    private ParticleSystem firedStream;
+
+    private int maxSpeed = 100;
+
+    //private Transform player;
+
+    void Start()
+    {
+        barrel = GetComponent<Transform>();
+
+        firedStream = Instantiate(waterStream, barrel.position, Quaternion.identity);
+
+        var particleEmission = firedStream.emission;
+        particleEmission.rateOverTime = 0;
+    }
+
+void Update()
+    {
+        Fire();
+    }
+
+    void Fire()
+    {
+        var particleEmission = firedStream.emission;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //firedStream = Instantiate(waterStream, barrel.position, Quaternion.identity);   //Creates particle system
+
+            transform.position = new Vector2(barrel.transform.position.x, barrel.transform.position.y);
+
+            particleEmission.rateOverTime = 90;
+
+
+        }
+        else if(Input.GetButtonUp("Fire1"))
+        {
+            //Destroy(firedStream);
+            particleEmission.rateOverTime = 0;
+        }
+    }
+    /*
+    float moveSpeed = 7f;
+    Rigidbody2D rb;
+
+    Fire target;
+    Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindObjectOfType<Fire>();
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if(col.gameObject.name.Equals("Fire"))
+        {
+            Debug.Log("Hit!");
+            Destroy(gameObject);
+        }
+    }
+    */
+}
