3b0d6823223728eb27af4243da2b9c65b6916b77
Marcus <marcuscesar1996@gmail.com>
2019-04-27 15:03:41 -0400

    Menu Update
    Coded script for infinite scrolling. Changed issues with jungle floor layering. Created animations for running newts in main menu.

---
+++ Assets/scrolling_bg.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrolling_bg : MonoBehaviour
+{
+    Material m_Material;
+    public float speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * speed, 0);
+   
+        m_Material.mainTextureOffset = offset;
+    }
+}
