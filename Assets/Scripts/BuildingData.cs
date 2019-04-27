ca8db317f1e5f5696e4b55b4452ee162dcdff1bb
Elisa <wheaton-cs@outlook.com>
2019-04-27 19:39:36 -0400

    added an optional timer
    a variable numfires was created in InitiateBuildings and records the number of fires initialized, can be used to call a function to end the game

--- Assets/Scripts/BuildingData.cs
+++ Assets/Scripts/BuildingData.cs
@@ -8,6 +8,7 @@ public class BuildingData : ScriptableObject
     public string bName;
     public int health;
     public int numWindows;
+    //public int numFires; 
     public List<float> spawnX; //size is numWindows
     public float spawnY; //all windows are on the same y axis
 }
